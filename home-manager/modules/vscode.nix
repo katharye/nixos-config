cb9f16e510058cbb33b8a35d635136e665315387
katharye <multiden007@gmail.com>
2025-10-25 15:40:08 +0300

    deleted docx extentions

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -8,13 +8,19 @@
    
     profiles.default = { 
       extensions = with pkgs.vscode-extensions; [
+        #Python
         ms-python.python
+        #C/C++
         ms-vscode.cpptools
+        #Nix
         jnoortheen.nix-ide
+        #Databases
+        yy0931.vscode-sqlite3-editor
       ];
 
       userSettings = {
         "security.workspace.trust.enabled" = false;
+        "files.autoSave" = "afterDelay";
       };
     };
   };
