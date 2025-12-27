75ef7f3bac9abb231e1ad1ffaea1fd724b2c272f
katharye <multiden007@gmail.com>
2025-12-27 14:53:02 +0300

    deleted

--- nixos/modules/virtualbox.nix
+++
@@ -1,7 +0,0 @@
-{
-    virtualisation.virtualbox.host.enable = true;
-    users.extraGroups.vboxusers.members = [ "katharye" ];
-
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-}
