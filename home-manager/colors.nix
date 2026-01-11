8c4843d021a74c393c7d0940d5bc035671b61ade
katharye <multiden007@gmail.com>
2026-01-11 17:26:24 +0300

    test colors autogenerate config

---
+++ home-manager/colors.nix
@@ -0,0 +1,21 @@
+{
+	background = "#1b1e25";
+	foreground = "#c6c6c8";
+	cursor = "#c6c6c8";
+	color0 = "#1b1e25";
+	color1 = "#66748E";
+	color2 = "#76839C";
+	color3 = "#7C88A2";
+	color4 = "#8893A9";
+	color5 = "#9BA4B7";
+	color6 = "#A0A8BA";
+	color7 = "#c6c6c8";
+	color8 = "#626878";
+	color9 = "#66748E";
+	color10 = "#76839C";
+	color11 = "#7C88A2";
+	color12 = "#8893A9";
+	color13 = "#9BA4B7";
+	color14 = "#A0A8BA";
+	color15 = "#c6c6c8";
+}
