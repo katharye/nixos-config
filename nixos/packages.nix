819cebcad8b9f43972f36c84dea7fbb6c033c411
katharye <multiden007@gmail.com>
2026-01-31 12:53:08 +0300

    Revert "updated to 25.11"
    This reverts commit 829c827a21b194eb32f89179fb14a5247e36de7a.

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -1,7 +1,7 @@
 { pkgs, pkgs-stable, ... }: {
   nixpkgs.config = {
     allowUnfree = true;
-    permittedInsecurePackages = ["electron-25.9.0"];
+    permittedInsecurePackages = ["python-2.7.18.8" "electron-25.9.0"];
   };
 
   environment.systemPackages = with pkgs; [
@@ -28,6 +28,7 @@
     SDL2
     SDL2.dev
     pkg-config
+    python
     python3
     docker
 
@@ -110,7 +111,7 @@
   fonts.packages = with pkgs; [
     jetbrains-mono
     noto-fonts
-    noto-fonts-color-emoji
+    noto-fonts-emoji
     twemoji-color-font
     font-awesome
     powerline-fonts
