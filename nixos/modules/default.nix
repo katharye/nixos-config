c1d8488d93a852dac7ab83cf4f76d6b3df71b334
katharye <multiden007@gmail.com>
2026-01-31 21:38:58 +0300

    renamed

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -2,11 +2,9 @@
   imports = [
     ./bootloader.nix
     ./bluetooth.nix
-    ./hyprland.nix
     ./network-manager.nix
     ./user.nix
     ./audio.nix
-    # ./gnome.nix
     ./amnezia.nix
     ./steam.nix
     ./virtualbox
